671a479fa1adea091653cf3f6b36c919474cd567
shuweitao <www.627080275@qq.com>
2016-11-21 17:54:22 +0800

    注册

--- app/src/main/java/com/swt/im/activity/LoginActivity.java
+++ app/src/main/java/com/swt/im/activity/LoginActivity.java
@@ -1,13 +1,10 @@
 package com.swt.im.activity;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
